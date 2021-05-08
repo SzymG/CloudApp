eb1e4c09c6af091defaae13ef1572df1f5a4bcbd
Szymon <szymon.gagorek@gmail.com>
2021-05-08 14:14:52 +0200

    Db fix

--- backend/api/controllers/atp.controller.js
+++ backend/api/controllers/atp.controller.js
@@ -5,8 +5,7 @@ let rawdata = fs.readFileSync(__dirname + '/../../configuration/data/db.json');
 let dbData = JSON.parse(rawdata);
 
 var con = mysql.createConnection({
-    host: "db",
-    port: '3306',
+    host: "mysql",
     user: dbData.user,
     password: dbData.password
 });
